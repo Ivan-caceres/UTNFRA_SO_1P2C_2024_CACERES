78a39e857afe522e8f7c777db19921fda0b16a54
Ivan-Caceres <ezeivan98@gmail.com>
2024-10-05 21:39:02 +0000

    ADD: resolucion del Punto A.

---
+++ RTA_SCRIPT_Examen_20241002/Punto_A.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Creamos las estructuras simétricas."
+
+mkdir -p ~/Examenes-UTN/alumno_{1..3}/parcial_{1..3}/ ~/Examenes-UTN/profesores/ 
+
+echo "Mostramos estructura."
+
+tree ~/Examenes-UTN/
+
