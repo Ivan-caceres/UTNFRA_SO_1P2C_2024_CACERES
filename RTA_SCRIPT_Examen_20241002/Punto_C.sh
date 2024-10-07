6ded8c715f8b56c3efa36952994abc8d759ec7fd
Ivan Caceres <ezeivan98@gmail.com>
2024-10-07 19:24:24 +0000

    FIX: Coreccion Punto C.

--- RTA_SCRIPT_Examen_20241002/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241002/Punto_C.sh
@@ -13,14 +13,14 @@ done
 sudo useradd -m -s /bin/bash -c "Creo user p1c2_2024_P1" -G p1c2_2024_gProfesores p1c2_2024_P1
 sudo usermod --password "$hash_user" p1c2_2024_P1
 
-for i in {1..3}:
+for i in {1..3};
 do
 	sudo chown p1c2_2024_A$i:p1c2_2024_gAlumno ~/Examenes-UTN/alumno_$i
 done 
 
 sudo chown p1c2_2024_P1:p1c2_2024_gProfesores ~/Examenes-UTN/profesores
 
-sudo chmod 730 ~/Examenes-UTN/alumno_1 
+sudo chmod 750 ~/Examenes-UTN/alumno_1 
 
 sudo chmod 760 ~/Examenes-UTN/alumno_2
 
@@ -30,6 +30,6 @@ sudo chmod 775 ~/Examenes-UTN/profesores
 
 for j in {1..3};
 do
-	sudo -u "p1c2_2024_A$j" bash -c "whoami > ~/Examenes-UTN/alumno_$j/validar.txt"
+	sudo -u "p1c2_2024_A$j" sh -c "whoami > ~/Examenes-UTN/alumno_$j/validar.txt"
 
 done
